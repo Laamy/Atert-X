8e10a2c3b037aa96a7abbc5470511abc8cb717f2
Laamy <User@DESKTOP-O86QES3>
2021-07-14 15:28:48 +1000

    Add project files.

---
+++ Atert-X/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Atert_X
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.godmodeBox = new System.Windows.Forms.CheckBox();
+            this.gameTicks = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // godmodeBox
+            // 
+            this.godmodeBox.AutoSize = true;
+            this.godmodeBox.Location = new System.Drawing.Point(12, 12);
+            this.godmodeBox.Name = "godmodeBox";
+            this.godmodeBox.Size = new System.Drawing.Size(72, 17);
+            this.godmodeBox.TabIndex = 0;
+            this.godmodeBox.Text = "Godmode";
+            this.godmodeBox.UseVisualStyleBackColor = true;
+            this.godmodeBox.CheckedChanged += new System.EventHandler(this.godmodeBox_CheckedChanged);
+            // 
+            // gameTicks
+            // 
+            this.gameTicks.Enabled = true;
+            this.gameTicks.Interval = 50;
+            this.gameTicks.Tick += new System.EventHandler(this.gameTicks_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 233);
+            this.Controls.Add(this.godmodeBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Atert X";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox godmodeBox;
+        private System.Windows.Forms.Timer gameTicks;
+    }
+}
+
---
+++ Atert-X/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Atert_X
+{
+    public partial class Form1 : Form
+    {
+        public Mem mem = new Mem();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            mem.OpenProcess("Nova Islands.exe"); // Open up the game process
+        }
+
+        public string health = "GameAssembly.dll+01BCB850,B8,0,2A8,50,25C";
+
+        private void gameTicks_Tick(object sender, EventArgs e)
+        {
+            if (godmodeBox.Checked)
+                mem.WriteMemory(health, "float", "200");
+        }
+
+        private void godmodeBox_CheckedChanged(object sender, EventArgs e) { }
+    }
+}
